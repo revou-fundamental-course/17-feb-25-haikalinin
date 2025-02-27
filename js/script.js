c1f317ebbdc68cecfc501cac7b056b28ccbb29d5
haikalinin <mustofahaikal007@gmail.com>
2025-02-27 23:21:14 +0700

    memperbaiki js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById('formInput').addEventListener('send', validasiForm);
+function validasiForm(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    if(name == "") {
+    alert("Nama tidak boleh kosong");
+    return false;
+    }
+
+    if(email == "") {
+    alert("Email tidak boleh kosong");
+    return false;
+    }
+
+    alert("Form berhasil disubmit");
+    return true;
+}
